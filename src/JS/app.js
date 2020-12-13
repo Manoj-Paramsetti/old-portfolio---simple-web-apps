452667074ce78a1c58b0e69ecdea87e0c6ab30aa
Manoj Paramsetti <39455174+Manoj-Paramsetti@users.noreply.github.com>
2020-12-13 18:41:15 +0530

    Update app.js

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,1 +1,3 @@
-window.addEventListener('load',()=>{ const preload =document.querySelector('.innerwrapper'); preload.classList.add('preload-finish');});
+window.addEventListener('load',()=>{ 
+const preload =document.querySelector('.innerwrapper'); 
+preload.classList.add('preload-finish');});
